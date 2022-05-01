f66f2f7c2f621789d1342d7e820468329bf60c4d
novacrazy <novacrazy@users.noreply.github.com>
2022-05-01 15:13:32 +0000

    deploy: 1651ef68de7e40cf957b3a57a6411a338ee19436

---
+++ implementors/client_sdk/models/permission/trait.SubPermission.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["client_sdk"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
