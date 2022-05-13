1a439c7158d0afeda0ddb28304fac264c76d9f5c
novacrazy <novacrazy@gmail.com>
2022-05-13 16:17:11 -0500

    CreateInvite -> CreatePartyInvite

--- src/api/commands/invite.rs
+++ src/api/commands/invite.rs
@@ -17,21 +17,4 @@ command! {
             pub nickname: Option<SmolStr>,
         }
     }
-
-    +struct CreateInvite -> Invite: POST("invite") {
-        ;
-        /// Infinite parameters may only be used with appropriate permissions
-        struct CreateInviteBody {
-            /// If `None`, invite has infinite uses
-            #[serde(default)]
-            pub max_uses: Option<u16>,
-
-            /// If `None`, invite has infinite duration
-            #[serde(default)]
-            pub duration: Option<u64>,
-
-            #[serde(default)]
-            pub description: Option<SmolStr>,
-        }
-    }
 }
--- src/api/commands/party.rs
+++ src/api/commands/party.rs
@@ -16,4 +16,23 @@ command! {
     +struct GetPartyInvites -> Vec<Invite>: GET("party" / party_id / "invites") {
         pub party_id: Snowflake,
     }
+
+    +struct CreatePartyInvite -> Invite: POST("party" / party_id / "invites") {
+        pub party_id: Snowflake,
+
+        ;
+        /// Infinite parameters may only be used with appropriate permissions
+        struct CreatePartyInviteBody {
+            /// If `None`, invite has infinite uses
+            #[serde(default)]
+            pub max_uses: Option<u16>,
+
+            /// If `None`, invite has infinite duration
+            #[serde(default)]
+            pub duration: Option<u64>,
+
+            #[serde(default)]
+            pub description: Option<SmolStr>,
+        }
+    }
 }
