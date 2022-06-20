434e41bb8d1f87c2acdeaa130716274df911bf73
novacrazy <novacrazy@gmail.com>
2022-06-20 07:43:05 -0500

    Add more server config parameters

--- src/models/config.rs
+++ src/models/config.rs
@@ -10,4 +10,17 @@ pub struct ServerConfig {
 
     /// Minimum user age (in years)
     pub min_age: u8,
+
+    /// If the serve should require HTTPS
+    pub secure: bool,
+
+    pub limits: ServerLimits,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
+pub struct ServerLimits {
+    pub max_upload_size: u64,
+    pub max_avatar_size: u32,
+    pub max_avatar_pixels: u32,
 }
