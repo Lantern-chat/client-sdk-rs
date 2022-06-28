b255b79714555b84f6567bbcd71dd310ad82027f
novacrazy <novacrazy@users.noreply.github.com>
2022-06-28 17:44:12 +0000

    deploy: 268572396aa052f1df1cc2ec16c632968ed71718

--- client_sdk/models/user/sidebar-items.js
+++ client_sdk/models/user/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ElevationLevel",""]],"struct":[["DateOfBirth",""],["Friend",""],["FriendFlags",""],["User",""],["UserFlags","NOTE: Remember to clear flag caches when they change"]]});
+initSidebarItems({"enum":[["ElevationLevel",""]],"struct":[["DateOfBirth",""],["Friend",""],["FriendFlags",""],["User",""],["UserFlags","NOTE: Remember to clear flag caches when they change"],["UserProfile",""]]});
