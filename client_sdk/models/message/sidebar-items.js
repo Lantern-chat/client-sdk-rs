fbd71e152979942855b7fa41f631a7b93053732e
novacrazy <novacrazy@users.noreply.github.com>
2022-07-27 12:55:15 +0000

    deploy: ef798b1c8d7afb2dd738a330ad3893928e0d4d6c

--- client_sdk/models/message/sidebar-items.js
+++ client_sdk/models/message/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["MessageKind",""],["Reaction",""]],"struct":[["Attachment",""],["Message",""],["MessageCreateForm",""],["MessageFlags",""],["ReactionFull",""],["ReactionShorthand",""]]});
+initSidebarItems({"enum":[["MessageKind",""],["Reaction",""]],"struct":[["Attachment",""],["Message",""],["MessageFlags",""],["PinFolder",""],["ReactionFull",""],["ReactionShorthand",""]]});
