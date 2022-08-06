c5827db648c6e435fc4ea87cbb9902c4268d39e0
novacrazy <novacrazy@gmail.com>
2022-08-06 18:38:02 -0500

    Cleanup

--- src/models/gateway.rs
+++ src/models/gateway.rs
@@ -77,7 +77,7 @@ bitflags::bitflags! {
     }
 }
 
-serde_shims::bitflags::impl_serde_for_bitflags!(Intent);
+serde_shims::impl_serde_for_bitflags!(Intent);
 impl_schema_for_bitflags!(Intent);
 impl_pg_for_bitflags!(Intent);
 
--- src/util/flags.rs
+++ src/util/flags.rs
@@ -3,7 +3,7 @@ macro_rules! impl_pg_for_bitflags {
         fn accepts(ty: &postgres_types::Type) -> bool {
             use postgres_types::Type;
 
-            *ty == match std::mem::size_of_val(&$id::empty()) {
+            *ty == match std::mem::size_of::<$id>() {
                 1 => Type::CHAR,
                 2 => Type::INT2,
                 4 => Type::INT4,
@@ -23,9 +23,7 @@ macro_rules! impl_pg_for_bitflags {
             impl<'a> FromSql<'a> for $id {
                 #[inline]
                 fn from_sql(ty: &Type, raw: &'a [u8]) -> Result<Self, Box<dyn Error + Sync + Send>> {
-                    Ok(Self::from_bits_truncate(
-                        FromSql::from_sql(ty, raw)?
-                    ))
+                    Ok(Self::from_bits_truncate(FromSql::from_sql(ty, raw)?))
                 }
 
                 impl_pg_for_bitflags!(@ACCEPTS $id);
@@ -33,7 +31,11 @@ macro_rules! impl_pg_for_bitflags {
 
             impl ToSql for $id {
                 #[inline]
-                fn to_sql(&self, ty: &Type, out: &mut BytesMut) -> Result<IsNull, Box<dyn Error + Sync + Send>>
+                fn to_sql(
+                    &self,
+                    ty: &Type,
+                    out: &mut BytesMut,
+                ) -> Result<IsNull, Box<dyn Error + Sync + Send>>
                 where
                     Self: Sized,
                 {
